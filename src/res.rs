30551fc1cae436b59816f216deaf9a05d6f6400b
Raum Dellamorte <elementalsin@gmail.com>
2018-09-06 02:13:31 -0800

    started goal to store files in bin

--- src/res.rs
+++ src/res.rs
@@ -3,11 +3,8 @@ use std::collections::HashMap;
 use std::sync::Mutex;
 
 lazy_static! {
-    static ref HASHMAP: Mutex<HashMap<u32, &'static str>> = {
+    static ref FILES: Mutex<HashMap<String, &'static [u8]>> = {
         let mut m = HashMap::new();
-        m.insert(0, "foo");
-        m.insert(1, "bar");
-        m.insert(2, "baz");
         Mutex::new(m)
     };    
 }

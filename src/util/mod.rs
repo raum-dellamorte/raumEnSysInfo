0acb1f796497dc3771430eaf3dcba468a87d579c
raum-dellamorte <raum-dellamorte@users.noreply.github.com>
2020-05-08 16:04:20 -0800

    Update mod.rs

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -6,21 +6,3 @@ pub mod rvertex;
 pub use util::rmatrix::Matrix4f;
 pub use util::rvector::{Vector2f, Vector3f};
 
-// #[derive(Debug, Copy, Clone)]
-// pub struct TransMat {
-//   pub transform: [f32; 16],
-// }
-
-// impl TransMat {
-//   pub fn new() -> Self {
-//     TransMat {
-//       transform: [1.0, 0.0, 0.0, 0.0,
-//                   0.0, 1.0, 0.0, 0.0,
-//                   0.0, 0.0, 1.0, 0.0,
-//                   0.0, 0.0, 0.0, 1.0_f32]
-//     }
-//   }
-//   pub fn len(&self) -> usize {
-//     self.transform.len()
-//   }
-// }

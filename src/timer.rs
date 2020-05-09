31c2bfa8a536b7a05efa4170be6bea8e6f2903d7
raum-dellamorte <raum-dellamorte@users.noreply.github.com>
2020-05-08 16:06:49 -0800

    updated to latest time crate

--- src/timer.rs
+++ src/timer.rs
@@ -1,32 +1,29 @@
 
-use time::SteadyTime;
+use time::Instant;
 
 pub struct Timer {
   pub delta: f32,
   pub fps: f32,
-  pub last: SteadyTime,
-  pub now: SteadyTime,
+  pub last: Instant,
+  pub now: Instant,
 }
 
 impl Timer {
   pub fn new() -> Self {
-    let tmp = SteadyTime::now();
+    let tmp = Instant::now();
     Timer {
       delta: 0.0667_f32,
       fps: 0_f32,
       last: tmp,
-      now: SteadyTime::now(),
+      now: Instant::now(),
     }
   }
   
   pub fn tick(&mut self) -> &Self {
     self.last = self.now;
-    self.now = SteadyTime::now();
+    self.now = Instant::now();
     let dur = self.now - self.last;
-    self.delta = match dur.num_microseconds() {
-      Some(t) => (t as f32) / 1000000_f32,
-      None => 0_f32,
-    };
+    self.delta = (dur.whole_microseconds() as f32) / 1000000_f32;
     self.fps = match self.delta > 0_f32 {
       true => 1_f32 / self.delta,
       false => 0_f32,

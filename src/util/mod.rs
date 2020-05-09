473bbe38ebd44d679b0526733d906fc0d3280bbb
raum-dellamorte <raum-dellamorte@users.noreply.github.com>
2020-05-08 16:04:51 -0800

    Update mod.rs

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -3,6 +3,21 @@ pub mod rvector;
 pub mod rmatrix;
 pub mod rvertex;
 
-pub use util::rmatrix::Matrix4f;
-pub use util::rvector::{Vector2f, Vector3f};
-
+pub use {
+  crate::util::{
+    maths::*, 
+    // rgl::*, 
+    rmatrix::Matrix4f, 
+    rvector::{
+      RVec, Vector2f, Vector3f, // Quaternion, 
+      XVEC, YVEC, ZVEC, //XVEC64, YVEC64, ZVEC64, 
+    },
+  },
+  // num::{ Float, NumCast, Zero, },
+  std::{ // Attach favourite standard library stuff here! #pub_use_abuse
+    rc::Rc,
+    cell::RefCell,
+    sync::{ Arc, Mutex, },
+    collections::{ HashMap, HashSet, },
+  },
+};
